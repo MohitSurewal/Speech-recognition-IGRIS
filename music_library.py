3e51094d764e7a3df104b7381b9d0878904731e4
Mohit Surewal <187611416+MohitSurewal@users.noreply.github.com>
2025-07-16 21:48:24 +0530

    music_library.py
    we make this library

---
+++ music_library.py
@@ -0,0 +1,7 @@
+music = {
+    "bulleya": "https://www.youtube.com/watch?v=wTgrZE9RWNY",
+    "bye": "https://www.youtube.com/watch?v=Eo-KmOd3i7s",
+    "deewana": "https://youtu.be/_4Ft9UIKzwk?si=yvMW7icFMLPuWl2x",
+    "chhaiya": "https://www.youtube.com/watch?v=K-pX4qwtAxA&list=RDMM&start_radio=1&rv=9yT4F8hzykY",
+    "mass" : "https://youtu.be/X6PLAysaevA?feature=shared"
+}
